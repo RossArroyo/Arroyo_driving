9d059d55eb4399aa2639fb81a36f64acf69ac1d5
RossArroyo <89609602+RossArroyo@users.noreply.github.com>
2021-10-29 11:57:13 -0400

    fixing migrations to create instructor table prior to courses

--- priv/repo/migrations/20211021005112_user.exs
+++ priv/repo/migrations/20211021005112_user.exs
@@ -2,15 +2,17 @@ defmodule ArroyoDriving.Repo.Migrations.User do
   use Ecto.Migration
 
   def change do
-    create table("user") do
-      add :username, :string null: false
-      add :password, :string null: false
-      add :name, :string null: false
-      add :phone, :string null: false
-      add :email, :string null: false
+    create table("users") do
+      add :username, :string, null: false
+      add :password, :string, null: false
+      add :name, :string, null: false
+      add :phone, :string, null: false
+      add :email, :string, null: false
 
 
     end
+
+
 
   end
 end
--- priv/repo/migrations/20211025224518_courses.exs
+++ priv/repo/migrations/20211025224518_courses.exs
@@ -2,10 +2,18 @@ defmodule ArroyoDriving.Repo.Migrations.Courses do
   use Ecto.Migration
 
   def change do
-    create table ("courses") do
-      add :title, :string, null false
-      add :price, :decimal, null false
-      add :instructor, :string, default: "No decided yet"
+    create table("instructors") do
+      add :name, :string, null: false
+      add :phone, :string
+      add :email, :string, null: false
+      timestamps()
+    end
+
+    create table("courses") do
+      add :title, :string, null: false
+      add :price, :decimal, null: false
+      add :instructor, references("instructors")
+      timestamps()
     end
   end
 end
